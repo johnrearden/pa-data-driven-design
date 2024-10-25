ba985a9e577e9be90eda6e9c04cab4b6a8ac93c6
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-10-25 11:19:17 +0000

    add first half of st Quick Project Summary

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -6,18 +6,20 @@ def page_summary_body():
 
     # text based on README file - "Dataset Content" section
     st.info(
-        f"**Project Terms & Jargon**\n"
-        f"* A **customer** is a person who consumes your service or product.\n"
-        f"* A **prospect** is a potential customer.\n"
-        f"* A **churned** customer is a user who has stopped using your product or service.\n "
-        f"* This customer has a **tenure** level, the number of months this person " 
-        f"has used our product/service.\n\n"
-        f"**Project Dataset**\n"
-        f"* The dataset represents a **customer base from a Telco company** "
-        f"containing individual customer data on the products and services "
-        f"(like internet type, online security, online backup, tech support), "
-        f"account information (like contract type, payment method, monthly charges) "
-        f"and profile (like gender, partner, dependents).")
+        f"**Terms & Jargon**\n"
+        f"* **General Aviation** is civil aviation other than large-scale passenger or freight operations.\n"
+        f"* A **Categorical value** is a value that falls into distinct categories or groups. For example," 
+        f" the Engine Type can be classified as piston, propjet, or jet.\n"
+        f"* A **Continuous numeric value** is a value that can take any real number\n"
+        f" (whole numbers and decimals). For example, the Cruise Speed can be 237,5 knots.\n\n"
+
+        f"**Dataset**\n"
+
+        f"* The dataset represents **857 Airplanes** in the category of **General Aviation** from"
+        f" [Kaggle](https://www.kaggle.com/datasets/heitornunes/aircraft-performance-dataset-aircraft-bluebook?select=Airplane_Complete_Imputation.csv) "
+        f"containing Meta data, 8 Design features, e.g. Dimensions and Weights "
+        f"and 17 Performance features, e.g. Cruise Speed, Range and Rate of Climb. "
+        f"Apart from three Categorical Design features the data set consists of continuous integers.")
 
     # Link to README file, so the users can have access to full project documentation
     st.write(
