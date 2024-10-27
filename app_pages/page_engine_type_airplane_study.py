10fa7b5ea57b9f5bbbebc27efb9c770f1b5994c8
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-10-27 18:22:26 +0000

    Add parallel plot to st page_engine_type_airplane_study

--- app_pages/page_engine_type_airplane_study.py
+++ app_pages/page_engine_type_airplane_study.py
@@ -2,7 +2,8 @@ import plotly.express as px
 import numpy as np
 from feature_engine.discretisation import ArbitraryDiscretiser
 import streamlit as st
-# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxfrom src.data_management import load_telco_data
+from src.data_management import load_airplane_data
+import plotly.express as px
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,13 +13,12 @@ sns.set_style("whitegrid")
 def page_engine_type_airplane_study_body():
 
     # load data
-    # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxdf = load_telco_data()
+    df = load_airplane_data()
 
-    # hard copied from churned customer study notebook
-    vars_to_study = ['Contract', 'InternetService',
-                     'OnlineSecurity', 'TechSupport', 'tenure']
+    # Hard copied from end of "Using 'Engine Type' as target variable" in "3A_airplane_engine_type_study"-notebook.
+    vars_to_study = ['Hmax', 'Vcruise', 'Vl', 'Vmax', 'Vstall']
 
-    st.write("### Multi Engine Airplane Study")
+    st.write("### Churned Customer Study")
     st.info(
         f"* The client is interested in understanding the patterns from the customer base "
         f"so that the client can learn the most relevant variables correlated "
@@ -53,7 +53,7 @@ def page_engine_type_airplane_study_body():
     )
 
     # Code copied from "02 - Churned Customer Study" notebook - "EDA on selected variables" section
-    df_eda = df.filter(vars_to_study + ['Churn'])
+    df_eda = df.filter(vars_to_study + ['Multi_Engine'])
 
     # Individual plots per variable
     if st.checkbox("Churn Levels per Variable"):
@@ -62,8 +62,8 @@ def page_engine_type_airplane_study_body():
     # Parallel plot
     if st.checkbox("Parallel Plot"):
         st.write(
-            f"* Information in yellow indicates the profile from a churned customer")
-        parallel_plot_churn(df_eda)
+            f"* Information in yellow indicates the profile from a Multi Engine Airplane")
+        parallel_plot_multi_engine(df_eda)
 
 
 # function created using "02 - Churned Customer Study" notebook code - "Variables Distribution by Churn" section
@@ -95,29 +95,72 @@ def plot_numerical(df, col, target_var):
     st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
 
 
-# function created using "02 - Churned Customer Study" notebook code - Parallel Plot section
-def parallel_plot_churn(df_eda):
+# function created using "3A_airplane_engine_type_study" notebook code - "Parallel Plot"-section
+def parallel_plot_multi_engine(df_eda):
 
-    # hard coded from "disc.binner_dict_['tenure']"" result,
-    tenure_map = [-np.Inf, 6, 12, 18, 24, np.Inf]
-    # found at "02 - Churned Customer Study" notebook
-    # under "Parallel Plot" section
-    disc = ArbitraryDiscretiser(binning_dict={'tenure': tenure_map})
+    # Define the mapping arrays
+    # Maps hard coded based on inspection of the histogram plots under "Variables Distribution by Multi Engine" in 3A_airplane_engine_type_study.
+    Hmax_map = [-np.Inf, 23000, 32000, 42000, 50000, np.Inf]
+    Vcruise_map = [-np.Inf, 250, 350, 450, 550, np.Inf]
+    Vl_map = [-np.Inf, 2000, 3000, 4000, np.Inf]
+    Vmax_map = [-np.Inf, 250, 350, 450, 550, np.Inf]
+    Vstall_map = [-np.Inf, 70, 90, 110, np.Inf]
+
+    # Combine all mappings into a single binning dictionary (Inbetween step necessary since we have multiple variables)
+    binning_dict = {
+        'Hmax': Hmax_map,
+        'Vcruise': Vcruise_map,
+        'Vl': Vl_map,
+        'Vmax': Vmax_map,
+        'Vstall': Vstall_map
+    }
+
+    # Initialize the ArbitraryDiscretiser with the combined binning dictionary
+    disc = ArbitraryDiscretiser(binning_dict=binning_dict)
+
+    # Fit and transform the DataFrame
     df_parallel = disc.fit_transform(df_eda)
 
-    n_classes = len(tenure_map) - 1
-    classes_ranges = disc.binner_dict_['tenure'][1:-1]
-    LabelsMap = {}
-    for n in range(0, n_classes):
-        if n == 0:
-            LabelsMap[n] = f"<{classes_ranges[0]}"
-        elif n == n_classes-1:
-            LabelsMap[n] = f"+{classes_ranges[-1]}"
-        else:
-            LabelsMap[n] = f"{classes_ranges[n-1]} to {classes_ranges[n]}"
+    # Access the binning dictionaries after fitting
+    if hasattr(disc, 'binner_dict_'):
+        print("Binning dictionary for Hmax:", disc.binner_dict_['Hmax'])
+        print("Binning dictionary for Vcruise:", disc.binner_dict_['Vcruise'])
+        print("Binning dictionary for Vl:", disc.binner_dict_['Vl'])
+        print("Binning dictionary for Vmax:", disc.binner_dict_['Vmax'])
+        print("Binning dictionary for Vstall:", disc.binner_dict_['Vstall'])
+    else:
+        print("binner_dict_ does not exist. Please check if the discretiser was fitted successfully.")
 
-    df_parallel['tenure'] = df_parallel['tenure'].replace(LabelsMap)
-    fig = px.parallel_categories(
-        df_parallel, color="Churn", width=750, height=500)
+    # Unsure if I need this line CHECK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    labels_map = {}
+
+    # Iterate over each variable in the binning dictionary
+    for variable in disc.binner_dict_.keys():
+        classes_ranges = disc.binner_dict_[variable][1:-1]  # Exclude -Inf and +Inf
+        n_classes = len(classes_ranges) + 1  # Number of intervals/classes
+    
+        # Initialize labels for this variable
+        variable_labels = {}
+    
+        for n in range(n_classes):
+            if n == 0:
+                variable_labels[n] = f"<{classes_ranges[0]}"
+            elif n == n_classes - 1:
+                variable_labels[n] = f"+{classes_ranges[-1]}"
+            else:
+                variable_labels[n] = f"{classes_ranges[n - 1]} to {classes_ranges[n]}"
+    
+        # Store the labels in the main labels_map
+        labels_map[variable] = variable_labels
+
+    # Replace the values in df_parallel for each variable using the corresponding labels from labels_map
+    for variable, labels in labels_map.items():
+        df_parallel[variable] = df_parallel[variable].replace(labels)
+
+    # Convert boolean to integer via replacing
+    df_parallel['Multi_Engine'] = df_parallel['Multi_Engine'].replace({True: 1, False: 0})
+
+    fig = px.parallel_categories(df_parallel, color="Multi_Engine")   #fig = px.parallel_categories(df_parallel, color="Multi_Engine", color_discrete_sequence=["blue", "orange"])
+    #fig.show(renderer='jupyterlab')
     # we use st.plotly_chart() to render, in notebook is fig.show()
     st.plotly_chart(fig)
