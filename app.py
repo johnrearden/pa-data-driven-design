39487b7c1b9dcf8035c68b156dd3b4ed4a8eefda
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-11-02 17:38:15 +0000

    Delete st page_ml_predict_engine_type

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from app_pages.page_wing_span_predictor import page_wing_span_predictor_body
 from app_pages.page_regression_playground import page_regression_playground_body
 from app_pages.page_domain_specific_analysis import page_domain_specific_analysis_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
-from app_pages.page_ml_predict_engine_type import page_ml_predict_engine_type_body
 from app_pages.page_ml_predict_wing_span import page_ml_predict_wing_span_body
 from app_pages.page_ml_cluster import page_ml_cluster_body
 
@@ -25,7 +24,6 @@ app.add_page("Wing Span Predictor", page_wing_span_predictor_body)
 app.add_page("Regression Playground", page_regression_playground_body)
 app.add_page("Domain Specific Analysis", page_domain_specific_analysis_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
-app.add_page("ML: Predict Engine Type", page_ml_predict_engine_type_body)
 app.add_page("ML: Predict Wing Span", page_ml_predict_wing_span_body)
 app.add_page("ML: Cluster Analysis", page_ml_cluster_body)
 
